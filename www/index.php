0263ccda8971ea6ecf1b22543a6771848dc23229
grafiaviles <grafiaviles@gmail.com>
2017-10-09 05:37:49 -0300

    footer en construccion

--- www/index.php
+++ www/index.php
@@ -3,29 +3,196 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Declima | Servicios y Productos para Sistemas de Climatización</title>
+    <meta name="description" content="Somos una empresa con experiencia en proyectos para sistemas de climatización como también en venta residencial, distribución comercial, 
+    instalación y mantenciones preventivas para equipos de aire acondicionado. | Declima * Buenas experiencias">
+    <!-- Styles -->
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="fonts/icons/icon-fonts.css">
-    <title>Declima | Buenas Experiencias</title>
+    <link rel="shortcut icon" href="img/favicon.png">
+    <!-- Data Seo -->
+    <meta name="author" content="César Avilés | Designer UX UI - Developer Frontend | www.laescala.cl">
+    <!-- Desactivar resaltado de vinculos en IE -->
+    <meta name="msapplication-tap-highlight" content="yes">
+    <!-- Verificador de google - propietarios del sitio -->
+    <meta name="google-site-verification" content="M4xuSIT66veOcZYE_RgpjDZ8XhTCQaE9n9bcY2wQq2s"/>
+    <!-- Theme color Android -->
+    <meta name="theme-color" content="#00587A">
+    <!-- Icono Opera y Chrome en Android * Clip web -->
+    <meta name="mobile-web-app-capable" content="yes"> <!-- Modo pantalla completa top 0 -->
+    <meta name="application-name" content="Declima">
+    <link rel="icon" sizes="192x192" href="img/touch/chrome-touch-icon.png">
+    <!-- Icono Safari en iOS * Clip web -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-title" content="Declima">
+    <link rel="apple-touch-icon" href="img/touch/touch-icon-iphone.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="img/touch/touch-icon-ipad.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="img/touch/touch-icon-iphone-retina.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/touch/touch-icon-ipad-retina.png">
+    <!-- Icono Windows 8 - Microsoft * Clip web -->
+    <meta name="msapplication-TileImage" content="img/touch/ms-touch-icon.png">
+    <meta name="msapplication-TileColor" content="#FFFFFF">
+    <!-- Open Graph data -->
+    <meta property="og:url" content="https://www.declima.cl"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:site_name" content="Declima"/>
+    <meta property="og:title" content=""/>
+    <meta property="og:description" content=""/>
+    <meta property="og:image" content="https://www.declima.cl/rrss/fb/fb-anuncio-wifi.png"/>
+    <meta property="og:image:height" content="628"/>
+    <meta property="og:image:width" content="1200"/>    
+    <meta property="og:image" content="https://www.declima.cl/rrss/fb/perfil-fb.png"/>
+    <meta property="og:image:height" content="770"/>
+    <meta property="og:image:width" content="770"/>
+    <!-- Open Google Share -->
+    <meta itemprop="name" content="">
+    <meta itemprop="description" content="">
+    <meta itemprop="image" content="http://www.declima.cl/rrss/fb/fb-anuncio-wifi.png">
+    <meta name="organization" content="Declima - Servicios y Productos para Sistemas de Climatización"/>
+    <meta name="locality" content="Santiago, Chile"/>
+
 </head>
 
 <body>
     <main>
-    <h1>HOLA MUNDO</h1>
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque 
-    penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
-    Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
     </main>
-
+    <!-- FOOTER -->
     <footer>
-    <h1>&copy; Declima Spa - 2017</h1>
-    </footer>
+    <!-- Centro de Ayuda -->
+    <div class="ayuda_user">
+			<div class="content_ayuda valign-wrapper row">
+				<div class="col s12 l7"><span class="white-text flow-text"><b>¿Necesitas Ayuda?</b> comunícate con nuestros ejecutivos</span></div>
+				<div class="col s12 l5">
+					<div class="row cont_btn_ayuda valign-wrapper">
+						<a href="tel:+56225858181" class="waves-effect waves-light btn tooltipped col s4 l4 data-position="bottom" data-delay="50" data-tooltip="Ll&aacute;menos 22 585 8181""><i class="icon-phone-call"></i></a>
+						<a href="" class="waves-effect waves-light btn tooltipped col s4 l4 data-position="bottom" data-delay="50" data-tooltip="Conversemos""><i class="icon-comment-circle"></i></a>
+                        <a href="mailto:lcivira@declima.cl,grafiaviles@gmail.com,amontero@declima.cl,dvenegas@declima.cl" class="waves-effect waves-light btn tooltipped col s4 l4 data-position="bottom" data-delay="50" data-tooltip="Escr&iacute;banos""><i class="icon-mail"></i></a>
+                        <a href="" class="waves-effect waves-light btn tooltipped col s4 l4 data-position="bottom" data-delay="50" data-tooltip="FAQ""><i class="icon-life-buoy"></i></a>
+					</div>
+				</div>
+			</div>
+	</div>
+    <!-- Informacion Desktop -->
+    <div class="container">
+    <div class="row cont_box_foot hide-on-med-and-down">
+      <div class="col s12 m3 l3 box_foot">
+            <div class="section">
+                <i class="icon-map-pin"></i>
+                <h5>Sala de Ventas</h5>
+                <p>Av. Artemio Gutiérrez #1268, <br>Santiago de Chile</p><br>
+                <p>Horarios de Atención:<br>Lunes a viernes de<br>
+                09:00am - 18:30pm hrs.
+                </p>
+            </div>
+            <div class="section">
+            <iframe class="section_maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.5964753483413!2d-70.63931634956197!3d-33.45981818067696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c56ed1ef9fd9%3A0x608ba36da8d8fe95!2sArtemio+Guti%C3%A9rrez+1268%2C+Santiago%2C+Regi%C3%B3n+Metropolitana!5e0!3m2!1ses-419!2scl!4v1507455161209" width="100%" height="160" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+            
+      </div>
+      <div class="col s12 m3 l3 box_foot">
+
+        <div class="section">
+            <i class="icon-headphones"></i>
+            <h5>Información Comercial</h5>
+            <p><span class="name_executive"></span> </p>
+            <a class="mail-executive" href="mailto:lcivira@declima.cl,grafiaviles@gmail.com,amontero@declima.cl,dvenegas@declima.cl,info@declima.cl">info@declima.cl</a><br>
+            <a href="tel:+56225858181">22 585 8181 -</a>
+            <a href="tel:+56225858181"> 22 585 8181</a>
+        </div>
+
+        <div class="section">
+        <p><span class="name_executive">Lenis Civira</span> <br>Ejecutiva Comercial</p>
+            <a class="mail-executive" href="mailto:lcivira@declima.cl,grafiaviles@gmail.com,amontero@declima.cl,dvenegas@declima.cl">lcivira@declima.cl</a>
+        </div>
+
+        <div class="section">
+            <p><span class="name_executive">Angela Montero</span> <br>Ejecutiva Comercial</p>
+            <a class="mail-executive" href="mailto:lcivira@declima.cl,grafiaviles@gmail.com,amontero@declima.cl,dvenegas@declima.cl">amontero@declima.cl</a>
+        </div>
+
+        <div class="section">
+            <p><span class="name_executive">Domingo Venegas</span> <br>Jefe de Proyectos</p>
+            <a class="mail-executive" href="mailto:lcivira@declima.cl,grafiaviles@gmail.com,amontero@declima.cl,dvenegas@declima.cl">dvenegas@declima.cl</a>
+        </div>
+      </div>
+      <div class="col s12 m3 l3 box_foot">
+        <div class="section">
+            <i class="icon-cog"></i>
+            <h5>Información de Servicios</h5>
+            <a href="#">Preguntas Frecuentes FAQ</a><br>
+            <a href="#">Cambios o Devoluciones</a><br>
+            <a href="#">Cálcula tu Despacho</a><br>
+            <a href="#">Terminos y Condiciones</a><br>
+            <a href="#">Garantías de Productos</a><br>
+            <a href="#">Cátalogos Online</a><br>
+        </div>
+        <div class="section">
+            <i class="icon-user-plus"></i>
+            <h5>Información para Usuarios</h5>
+            <a href="#">¿Cómo puedo comprar?</a><br>
+            <a href="#">Cálcula tu Aire Acondicionado</a><br>
+            <a href="#">Blog de aprendisaje</a><br>
+            <a href="#">Trabaja con nosotros</a><br>
+        </div>
+      </div>
+      <div class="col s12 m3 l3 box_foot">
+        <div class="section">
+            <i class="icon-at-sign"></i>
+            <h5>Declima News</h5>
+            <p>Regístrate sólo con tu correo y recibe nuestras ofertas, noticias, novedades y mucho más.</p>
+            <div class="section section-form">
+                <form class="form-news" autocomplete="off">
+                    <div class="input-field inline">
+                    <input id="email-news" type="email" class="input-email-news validate">
+                    <label class="label-email-news" for="email-news" data-error="Invalido" data-success="Correcto">Email</label>
+                    <input type="submit" value="Enviar" class="submit-email-news">
+                    </div>
+                </form>
+            </div>
+            <div class="section section_redes_footer">
+                <i class="icon-share"></i>
+                <h5>Síguenos tu también</h5>
+                <a class="icon-redes" href="https://www.facebook.com/declimachile" target="_blank"><i class="icon-footer icon-facebook"></i></a>
+                <a class="icon-redes" href="https://www.instagram.com/declima/" target="_blank"><i class="icon-footer icon-instagram"></i></a>
+                <a class="icon-redes" href="https://plus.google.com/u/0/112998915048326285751" target="_blank"><i class="icon-footer icon-brand10"></i></a>
+            </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    <!-- Informacion Mobile -->
+    <div class="container-full hide-on-large-only">
+    <div class="row cont_box_foot_mobile">
+    <ul class="collapsible" data-collapsible="accordion">
+        <li>
+            <div class="collapsible-header"><i class="icon-map-pin"></i>Sala de Ventas</div>
+            <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+            <li>
+            <div class="collapsible-header"><i class="icon-headphones"></i>Ejecutivos comerciales</div>
+            <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+            <li>
+            <div class="collapsible-header"><i class="icon-user-plus"></i>Información de Usuarios</div>
+            <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+            <li>
+            <div class="collapsible-header"><i class="icon-at-sign"></i>Declima News</div>
+            <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+        </li>
+    </ul>
+    </div>
+    </div>
+</footer>
 
 
 <script src="lib/jquery-3.2.1.min.js"></script>
 <script src="lib/materialize.min.js"></script>
 <script src="js/dist/effects.js"></script>
+<?php include("include/metricas.php"); ?>
 </body>
 
 </html>
